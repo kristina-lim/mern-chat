09746730eab8b8824833a1a0c003a47a96ec1428
kristina-lim <kristinahlim@gmail.com>
2023-02-28 15:45:15 -0800

    Baby step and delete css component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
+    <div className="bg-red-500">
       test
     </div>
   )
