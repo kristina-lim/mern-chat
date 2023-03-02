855f2d04a2b3f1be61c1a426daa0d31e0320e4c0
kristina-lim <kristinahlim@gmail.com>
2023-03-01 16:22:13 -0800

    Center register form

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 export default function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto">
+      <form className="w-64 mx-auto mb-12">
         <input 
           value={username}
           onChange={evt => setUsername(evt.target.value)}
